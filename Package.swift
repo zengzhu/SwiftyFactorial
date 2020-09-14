be1d6a07e21cd26b075efc5b050b4e52f82eecf3
john <zengzhu@gmail.com>
2020-09-14 17:28:09 +0800

    save changes.

--- Package.swift
+++ Package.swift
@@ -1,8 +1,23 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
 	name: "SwiftyFactorial",
-	dependencies: [
-		.Package(url: "https://github.com/aciidb0mb3r/CGetFactorial", majorVersion: 1)
-	]
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .executable(
+            name: "SwiftyFactorial",
+            targets: ["SwiftyFactorial"]),
+    ],
+    dependencies: [
+         .package(url: "../clib",  from: "1.3.6")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "SwiftyFactorial",
+            dependencies: [ "Once"]),
+    ]
 )
--- Sources/main.swift
+++
@@ -1,4 +0,0 @@
-import CGetFactorial
-
-let result = factorial(20)
-print("factorial of 20 = \(result)")
